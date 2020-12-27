d1cfef2b2dab1c6caace5464bf1039750805311f
tedi4t <tedi4t@gmail.com>
2020-12-28 01:05:53 +0200

    add query coder

---
+++ src/utils/queryCoder.js
@@ -0,0 +1,11 @@
+export default (queryObj = {}) => {
+  console.log(queryObj);
+  let query = '?';
+  const options = [];
+  for (const key in queryObj) {
+    options.push(`${key}=${queryObj[key]}`);
+  }
+  query += options.join('&');
+  
+  return query;
+}
