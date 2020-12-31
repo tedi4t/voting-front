4701ad373cbd9aa026e33f4908120ffb3ed22064
tedi4t <tedi4t@gmail.com>
2020-12-31 22:13:35 +0200

    add list of votings

--- src/modules/pages/votingList/index.js
+++ src/modules/pages/votingList/index.js
@@ -1,24 +1,64 @@
-import React, { useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from 'react-router-dom';
 import useFetch from '../../hooks/useFetch';
 import ErrorMessage from '../../components/errorMessage';
 import LoadingMessage from '../../components/loadingMessage';
 import StartEndDate from "../../components/startEndDate";
+import { userContext } from "../../contexts/user";
 
 export default () => {
   const [{ response, isLoading, error}, doFetch] = useFetch('/voting/all');
+  const [userState] = useContext(userContext);
+  const [searchText, setSearchText] = useState('');
+
+  const status =  userState.user && userState.user.status;
 
   useEffect(() => {
-    doFetch({
-      queryFields:{
-        limit: 10,
-        offset: 0
-      }
-    });
+    doFetch({ queryFields: {
+      limit: 10,
+      offset: 0,
+    } });
   }, [doFetch]);
+
+  const handleSearchForm = () => {
+    doFetch({
+      queryFields: {
+      limit: 10,
+      offset: 0,
+      searchText
+    }});
+  }
 
   return (
     <div className = "container">
+      {status === 1 && (
+        <button 
+          className = "btn btn-lg btn-secondary mt-2"
+          style = {{
+            borderRadius: "50px",
+          }}
+        >
+          <i className = "fas fa-plus"></i> &nbsp;
+          Додати нове голосування
+        </button>
+      )}
+      <div className = "mt-3">
+        <input 
+          type = "text"
+          className = "form-group form-control w-75 float-left "
+          placeholder = "Введіть назву голосування"
+          value = {searchText}
+          onChange = {e => setSearchText(e.target.value)}
+        />
+        <div className = "w-25 float-left pl-3">
+          <button 
+            onClick = { handleSearchForm }
+            className = "btn btn-warning w-100"
+          >
+            Знайти
+          </button>
+        </div>
+      </div>
       {error && <ErrorMessage error = { error } />}
       {isLoading && <LoadingMessage />}
       {response && (
