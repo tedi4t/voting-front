8fd66bc1399ee276bb008b9e54b039023229fc69
tedi4t <tedi4t@gmail.com>
2021-01-04 23:55:15 +0200

    add query decoder

---
+++ src/utils/queryDecoder.js
@@ -0,0 +1,17 @@
+export default (str = '') => {
+    const strArr = str.split('');
+    strArr.shift();
+    const strTrimmed = strArr.join('');
+    const queryArgs = strTrimmed.split('&').map(str => {
+      const keyVal = str.split('=');
+      return [keyVal[0], keyVal[1]];
+    });
+    const queryObj = {};
+    queryArgs.forEach(arg => {
+      const key = arg[0];
+      const val = arg[1];
+      queryObj[key] = val;
+    })
+    return queryObj;
+  }
+  
