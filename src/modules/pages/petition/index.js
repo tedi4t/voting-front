de8e4d85a2093974c23de863110baed04c1d06ad
tedi4t <tedi4t@gmail.com>
2021-01-05 12:14:47 +0200

    add petition page

---
+++ src/modules/pages/petition/index.js
@@ -0,0 +1,9 @@
+import React, { useEffect, Fragment, useState, useContext } from "react";
+
+export default () => {
+  return (
+    <div className = "container">
+      Petition
+    </div>
+  )
+}
