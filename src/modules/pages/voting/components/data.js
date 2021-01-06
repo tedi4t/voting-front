e43809aead2549b6fcd5798f1c1b32a5095ff032
tedi4t <tedi4t@gmail.com>
2021-01-06 14:28:19 +0200

    add component with results for separate district

---
+++ src/modules/pages/voting/components/data.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default ({ mouseLocation, content }) => {
+  return (
+    <div 
+        id = "data"
+        className = 'border border-warning px-3 py-2'
+        style = {{
+          width: "500px",
+          display: "block",
+          position: "absolute",
+          top: `${mouseLocation.y}px`,
+          left: `${mouseLocation.x}px`,
+          zIndex: "10",
+          backgroundColor: "rgba(255, 255, 255, 0.8)",
+          borderBottomLeftRadius: "15px",
+          borderBottomRightRadius: "15px",
+          borderTopRightRadius: "15px",
+        }}  
+      >
+        { content }
+      </div>
+  )
+}
