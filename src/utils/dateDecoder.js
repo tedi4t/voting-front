b8cea3d8eb489f23d727384e54a712fe7782832a
tedi4t <tedi4t@gmail.com>
2021-01-21 20:50:48 +0200

    added timezone offset

--- src/utils/dateDecoder.js
+++ src/utils/dateDecoder.js
@@ -1,8 +1,19 @@
 import makeTwoDigit from "./makeTwoDigit";
 
+const changeTimezone = date => {
+  if (typeof date !== Date)
+    date = new Date(date);
+
+  const offset = date.getTimezoneOffset();
+  date.setMinutes(date.getMinutes() + offset);
+  return date;
+}
+
 export default date => {
   if (!typeof date !== Date)
-  date = new Date(date);
+    date = new Date(date);
+
+  date = changeTimezone(date);
 
   let dateDecoded = {
     day: date.getDate(),
